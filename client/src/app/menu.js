15b00a7b116832bf834fbad44aae28ed53d1acce
brandon600 <us-guest@us-guests-MacBook-Pro.local>
2023-11-12 01:59:12 -0600

    experimented

---
+++ client/src/app/menu.js
@@ -0,0 +1,25 @@
+const menu = {
+    items: [
+      {
+        name: "Appetizers",
+        description: "\nTomato Mozzarella Caprese\nBruschetta\nCalamari Fries",
+      },
+      {
+        name: "Soup/Salad",
+        description: "\nGarden Salad\nCeasar Salad\nTomato Basil Soup",
+      },
+      {
+        name: "Main Course",
+        description: "\nGrilled Chicken Piccata\nClassic Spaghetti with Meatballs\nPrime Steak with Mashed Potato\nVeggie Pizza",
+      },
+      {
+        name: "Seafood",
+        description: "\nShrimp Scampi\nGrilled Salmon with Asparagus\nLobster Tail",
+      },
+      {
+        name: "Vegetarian",
+        description: "\nAdd your vegetarian options here",
+      },
+      // Add more menu categories and items as needed
+    ],
+  };
